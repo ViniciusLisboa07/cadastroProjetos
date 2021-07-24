adbbf1341526a98ff37a8051b80c7092d0ee3a4e
Vinícius Lisboa <viniciuslisboa1001@gmail.com>
2021-07-24 14:06:14 -0300

    connectiong to database

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const app = require('./app');
+const mongoose = require('mongoose');
 
-app.set('port', 7777);
+require('dotenv').config({path:'VARIEBLES.env'})
+ 
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+console.log(process.env.DATABASE)
+mongoose.Promise = global.Promise;
+ 
+mongoose.connection.on('error', (error) => {
+    console.error("ERRO: "+error.message);
+});
+
+
+app.set('port', process.env.port || 7777);
+  
+const server = app.listen(app.get('port'), ( ) => {
+    console.log("Servidor rodando na porta: " + server.address().port);
+})
